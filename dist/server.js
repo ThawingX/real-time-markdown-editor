6b6886b0b6b22a974ffdc83be1e8c4d03b0718bd
Dylan Long <thawingx@gmail.com>
2024-05-27 22:39:57 +0800

    feat: finish the static page,to import redis for real-time share content between all of users

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// use ejs view engine as our template engine
+app.set('view engine', 'ejs');
+// serve static files folder
+app.use(express_1.default.static(__dirname + '/public'));
+// routes for app
+app.get('/', (req, res) => {
+    // render home page by specifying the view file name in views folder
+    res.render('home');
+});
+const port = process.env.PORT || 3030;
+app.listen(port);
--- server.ts
+++ server.ts
@@ -12,8 +12,15 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
   // render home page by specifying the view file name in views folder
   res.render('home')
+
+})
+app.get('/pad', (req, res) => {
+  res.render('pad')
 })
 
 
 const port = process.env.PORT || 3030
 app.listen(port)
+console.log(`Server started on http://localhost:${port}`)
+
+
